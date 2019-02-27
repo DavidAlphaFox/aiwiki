a9d88aab97064823665fe3378986d1fdfec173de
David.Gao <david.alpha.fox@gmail.com>
2019-02-27 22:44:18 +0800

    split module

---
+++ src/base/config.lisp
@@ -0,0 +1,49 @@
+(in-package :cl-user)
+(defpackage aiwiki.config
+  (:use :cl)
+  (:import-from :envy
+                :config-env-var
+                :defconfig)
+  (:export :config
+           :*application-root*
+           :*static-directory*
+           :*template-directory*
+           :appenv
+           :developmentp
+           :productionp))
+(in-package :aiwiki.config)
+
+(setf (config-env-var) "APP_ENV")
+
+(defparameter *application-root*   (asdf:system-source-directory :aiwiki))
+(defparameter *static-directory*   (merge-pathnames #P"static/" *application-root*))
+(defparameter *template-directory* (merge-pathnames #P"templates/" *application-root*))
+
+(defconfig :common
+    `(:debug t
+      :databases ((:maindb :postgres
+                   :database-name "fswikidb"
+                   :username "postgres"
+                   :host "127.0.0.1"
+                   ))))
+
+(defconfig |development|
+  '())
+
+(defconfig |production|
+  '())
+
+(defconfig |test|
+  '())
+
+(defun config (&optional key)
+  (envy:config #.(package-name *package*) key))
+
+(defun appenv ()
+  (uiop:getenv (config-env-var #.(package-name *package*))))
+
+(defun developmentp ()
+  (string= (appenv) "development"))
+
+(defun productionp ()
+  (string= (appenv) "production"))
---
+++ src/base/db.lisp
@@ -0,0 +1,28 @@
+(in-package :cl-user)
+(defpackage aiwiki.db
+  (:use :cl)
+  (:import-from :aiwiki.config
+                :config)
+  (:import-from :datafly
+                :*connection*
+                :connect-cached)
+  (:export :connection-settings
+           :db
+           :with-connection
+           :with-transaction))
+(in-package :aiwiki.db)
+
+(defun connection-settings (&optional (db :maindb))
+  (cdr (assoc db (config :databases))))
+
+(defun db (&optional (db :maindb))
+  (apply #'connect-cached (connection-settings db)))
+
+(defmacro with-connection (conn &body body)
+  `(let ((*connection* ,conn))
+     ,@body))
+
+(defmacro with-transaction (conn &body body)
+  `(let ((*connection* ,conn))
+     (cl-dbi:with-transaction *connection*
+       ,@body)))
