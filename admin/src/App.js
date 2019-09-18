49a67dddcd3d59a3222ad0970fa1c7852973f1d8
David.Gao <david.alpha.fox@gmail.com>
2019-09-18 19:05:39 +0800

    Use for-editor

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
+import Editor from 'for-editor';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'XMPP';
+  const [content, setContent] = React.useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="section">
+      <div className="columns">
+        <div className="column is-one-fifth">
+        </div>
+        <div className="column">
+          <div className="field">
+            <label className="label">
+              文章标题
+        </label>
+            <div className="control">
+              <input
+                className="input"
+                value={title} />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">
+              文章内容
+        </label>
+            <div className="control">
+              <Editor
+                height="500px"
+                toolbar={{
+                  preview: true
+                }}
+                onChange={value => setContent(value)}
+                value={content}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
