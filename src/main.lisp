c39263ac6592f310696e58088a0ec5e9f6776569
David.Gao <david.alpha.fox@gmail.com>
2019-09-22 22:25:15 +0800

    Fix starting progress

--- src/main.lisp
+++ src/main.lisp
@@ -4,16 +4,45 @@
   (:use :cl)
   (:import-from
    :aiwiki.base.config
-   :config)
+   :config
+   :productionp
+   :*static-directory*)
   (:import-from
    :clack
    :clackup)
+  (:import-from
+   :lack.builder
+   :builder)
+  (:import-from
+   :ppcre
+   :scan
+   :regex-replace)
+  (:import-from
+   :aiwiki.view
+   :*web*)
   (:export :start
            :stop))
 (in-package :aiwiki)
 
-(defvar *appfile-path*
-  (asdf:system-relative-pathname :aiwiki #P"app.lisp"))
+;;(defvar *appfile-path*
+;;  (asdf:system-relative-pathname :aiwiki #P"app.lisp"))
+
+(defparameter *app*
+  (builder
+   (:static
+    :path(lambda (path)
+           (if (ppcre:scan "^(?:/images/|/css/|/js/|/robot\\.txt$|/favicon\\.ico$)" path)
+             path
+             nil))
+    :root *static-directory*)
+   (if (productionp) nil :accesslog)
+   (if (getf (config) :error-log) `(:backtrace :output ,(getf (config) :error-log)) nil)
+   :session (if (productionp) nil
+              (lambda (app)
+                (lambda (env)
+                  (let ((datafly:*trace-sql* t)) ;; 如果非生产环境提供sql的追踪
+                    (funcall app env)))))
+   *web*))
 
 (defvar *handler* nil)
 
@@ -24,7 +53,7 @@
       (restart-server ()
         :report "Restart the server"
         (stop))))
-  (setf *handler* (apply #'clackup *appfile-path* args)))
+  (setf *handler* (apply #'clackup *app* args)))
 
 (defun stop ()
   (prog1
