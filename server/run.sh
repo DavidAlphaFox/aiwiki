7434ee56104986e5d317d74cbda13d2bf87110e1
David.Gao <david.alpha.fox@gmail.com>
2019-10-01 10:07:47 +0800

    Update run script

--- server/run.sh
+++ server/run.sh
@@ -6,5 +6,5 @@ export APP_ENV="production"
         --disable-debugger \
         --eval '(pushnew #P"/srv/aiwiki/" asdf:*central-registry*)' \
         --eval '(progn (ql:quickload :aiwiki) (ql:quickload :slynk))' \
-        --eval '(progn (aiwiki:start :mode :production :address "127.0.0.1" :server :woo) \
-                       (slynk:create-server :port 4005 :style :spawn :dont-close t))'
+        --eval '(progn (aiwiki:start :mode :production :address "127.0.0.1" :server :woo)
+                (slynk:create-server :port 4005 :style :spawn :dont-close t))'
