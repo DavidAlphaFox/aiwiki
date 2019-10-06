667e72866be93dc3815d3ab28d6a380bab1f5656
David.Gao <david.alpha.fox@gmail.com>
2019-10-06 13:33:03 +0800

    Update make shell

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -r ./dist
-mkdir -p ./dist/aiwiki/admin
+mkdir -p ./dist/static/app/admin
 cd ./admin && yarn && yarn build && cd ..
-cp -r ./admin/build/* ./dist/aiwiki/admin/
-cp -r ./server/static/* ./dist/aiwiki/
-cp -r ./server ./dist
+cp -r ./admin/build/* ./dist/static/app/admin/
+cp -r ./server/static/* ./dist/static/app
+cp -r ./server ./dist/aiwiki
