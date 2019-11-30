63ea3b1c89b08281099f484a7ed04e09e8ed13e2
David.Gao <david.alpha.fox@gmail.com>
2019-12-01 01:20:59 +0800

    Using new color scheme

--- src/aiwiki_view.erl
+++ src/aiwiki_view.erl
@@ -1,9 +1,6 @@
 -module(aiwiki_view).
 -export([render/3]).
 
-yield(<<>>,Context)->
-  YieldTemplate = maps:get(<<"yield_template">>,Context),
-  ai_mustache:render(YieldTemplate,Context);
 yield(Template,Context)->
   ai_mustache:render(Template,Context).
 
@@ -11,7 +8,6 @@ site_title(Acc,Context)->
   case  maps:get(<<"title">>,Context,undefined) of
     undefined -> Acc;
     Title ->
-      io:format("Brand:~p~n",[Acc]),
       case erlang:byte_size(Acc) of
         0 -> Title;
         _ -> <<Title/binary,"-",Acc/binary>>
@@ -21,9 +17,8 @@ site_title(Acc,Context)->
 render(Template,Req,State)->
   Context = maps:get(context,State,#{}),
   LayoutContext = Context#{
-                            <<"yield">> => fun yield/2,
-                            <<"site_title">> => fun site_title/2,
-                            <<"yield_template">> => Template
+                            <<"yield">> => [fun yield/2,Template],
+                            <<"site_title">> => fun site_title/2
                           },
   Body = ai_mustache:render(<<"_layout/default">>,LayoutContext),
   Req0 = cowboy_req:reply(200, #{
