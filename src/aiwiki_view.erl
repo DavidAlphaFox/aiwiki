ec15c76963c529e1c91c56ce1239a45f6f02be2d
David.Gao <david.alpha.fox@gmail.com>
2019-11-30 20:58:19 +0800

    using new ui

--- src/aiwiki_view.erl
+++ src/aiwiki_view.erl
@@ -7,9 +7,24 @@ yield(<<>>,Context)->
 yield(Template,Context)->
   ai_mustache:render(Template,Context).
 
+site_title(Acc,Context)->
+  case  maps:get(<<"title">>,Context,undefined) of
+    undefined -> Acc;
+    Title ->
+      io:format("Brand:~p~n",[Acc]),
+      case erlang:byte_size(Acc) of
+        0 -> Title;
+        _ -> <<Title/binary,"-",Acc/binary>>
+      end
+  end.
+
 render(Template,Req,State)->
   Context = maps:get(context,State,#{}),
-  LayoutContext = Context#{ <<"yield">> => fun yield/2, <<"yield_template">> => Template },
+  LayoutContext = Context#{
+                            <<"yield">> => fun yield/2,
+                            <<"site_title">> => fun site_title/2,
+                            <<"yield_template">> => Template
+                          },
   Body = ai_mustache:render(<<"_layout/default">>,LayoutContext),
   Req0 = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"text/html; charset=utf-8">>
