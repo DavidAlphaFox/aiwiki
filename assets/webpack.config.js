e8c32e1b71009efa7647d5cc588259d9aa629b93
David.Gao <david.alpha.fox@gmail.com>
2019-12-01 19:23:53 +0800

    Add Styles

---
+++ assets/webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+module.exports = {
+  entry: './src/styles.css',
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use:  [
+        MiniCssExtractPlugin.loader,
+        "css-loader", "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: '../../public/css/styles.css',
+      chunkFilename: '../../public/css/styles.css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  optimization: {
+    minimizer: [ new OptimizeCSSAssetsPlugin({})],
+  },
+}
