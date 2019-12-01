083f19b0955253f306c1d0a1af6fe09402de0478
David.Gao <david.alpha.fox@gmail.com>
2019-12-01 15:37:49 +0800

    Pagination Query

--- src/aiwiki_page_model.erl
+++ src/aiwiki_page_model.erl
@@ -1,6 +1,9 @@
 -module(aiwiki_page_model).
 -export([wakeup/1,sleep/1,schema/0,attributes/0]).
 -export([build/3]).
+-export([pagination/2,pagination/3]).
+
+-include_lib("stdlib/include/qlc.hrl").
 
 build(Title,Intro,Content)->
   [
@@ -11,10 +14,29 @@ build(Title,Intro,Content)->
    {published_at, calendar:universal_time()},
    {topic_id,0}
   ].
+pagination(PageIndex,PageCount)->pagination(PageIndex,PageCount,true).
+pagination(PageIndex,PageCount,Published)->
+  Offset = PageIndex * PageCount,
+  F = fun() ->  
+      Q = qlc:q([P || P <- mnesia:table(page),
+        erlang:element(6,P) == Published]),  
+      Q0 = qlc:keysort(7, Q, [{order, descending}]),
+      QC = qlc:cursor(Q0),
+      Answers = 
+        if Offset == 0 -> qlc:next_answers(QC,PageCount);
+          true -> 
+            qlc:next_answers(QC,Offset),
+            qlc:next_answers(QC,PageCount)
+        end,
+      qlc:delete_cursor(QC),
+      Answers
+    end,  
+  mnesia:transaction(F).  
 
 sleep(PropList)-> maps:from_list(PropList).
 
 wakeup(Fields)-> maps:to_list(Fields).
+
 attributes()->
   Fields = fields(),
   lists:map(fun(F)-> ai_db_schema:field_name(F) end,Fields).
