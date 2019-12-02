a0a5f7cdde780cc4e0ebd387180538c533bd3ec6
David.Gao <david.alpha.fox@gmail.com>
2019-12-02 11:30:56 +0800

    database dump and rss

--- src/controller/aiwiki_rss_controller.erl
+++ src/controller/aiwiki_rss_controller.erl
@@ -2,9 +2,11 @@
 -export([init/2]).
 init(Req,State)->
   Pages = aiwiki_page_model:pagination(0,5),
+  HostConf = ai_db:fetch(site,<<"host">>),
+  Host = proplists:get_value(value,HostConf),
   Pages0 = lists:map(fun(M) -> 
       M0 = aiwiki_page_helper:view_model(M),
-      Url = aiwiki_page_helper:url(M0),
+      Url = aiwiki_page_helper:url(Host,M0),
       M0#{<<"url">> => Url}
     end,Pages),
   aiwiki_view:render(<<"xml/rss">>,<<"application/xml">>,Req,State#{context => #{
--- src/manager/aiwiki_db_manager.erl
+++ src/manager/aiwiki_db_manager.erl
@@ -2,10 +2,56 @@
 -module(aiwiki_db_manager).
 -export([load_page/1,dump_page/1]).
 -export([load_topic/1]).
+-export([dump_database/1,load_database/1]).
 
 load_page(File)->
   {ok,FileData} = file:read_file(File),
   Json = jsx:decode(FileData),
+  restore_page(Json).
+
+dump_page(File) ->
+  Page = snapshot_page(),
+  Bin = jsx:encode(Page),
+  file:write_file(File,Bin).
+
+load_topic(File)->
+  {ok,FileData} = file:read_file(File),
+  Json = jsx:decode(FileData),
+  restore_topic(Json).
+
+
+dump_database(File)->
+  Page = snapshot_page(),
+  Topic = snapshot_topic(),
+  Site = snapshot_site(),
+  DB = [
+    {<<"site">>,Site},
+    {<<"page">>,Page},
+    {<<"topic">>,Topic}
+  ],
+  Bin = jsx:encode(DB),
+  file:write_file(File,Bin).
+
+load_database(File) -> 
+  {ok,FileData} = file:read_file(File),
+  Json = jsx:decode(FileData),
+  Page = proplists:get_value(<<"page">>,Json),
+  Site = proplists:get_value(<<"site">>,Json),
+  Topic = proplists:get_value(<<"topic">>,Json),
+  restore_page(Page),
+  restore_topic(Topic),
+  restore_site(Site).
+
+snapshot_page()->
+  All = ai_db:find_all(page),
+  lists:map(fun(I)->
+      PublishedAt = proplists:get_value(published_at,I),
+      PublishedAt0 = calendar:datetime_to_gregorian_seconds(PublishedAt),
+      lists:keyreplace(published_at,1,I,{published_at,PublishedAt0})
+    end,All).
+snapshot_topic() -> ai_db:find_all(topic).
+snapshot_site() -> ai_db:find_all(site).
+restore_page(Json)->
   lists:foreach(fun(I) -> 
     ID = proplists:get_value(<<"id">>,I),
     Title = proplists:get_value(<<"title">>,I),
@@ -30,19 +76,8 @@ load_page(File)->
           ],
     ai_db:persist(page,Item)
     end,Json).
-dump_page(File) ->
-  All = ai_db:find_all(page),
-  All0 = lists:map(fun(I)->
-      PublishedAt = proplists:get_value(published_at,I),
-      PublishedAt0 = calendar:datetime_to_gregorian_seconds(PublishedAt),
-      lists:keyreplace(published_at,1,I,{published_at,PublishedAt0})
-    end,All),
-  Bin = jsx:encode(All0),
-  file:write_file(File,Bin).
 
-load_topic(File)->
-  {ok,FileData} = file:read_file(File),
-  Json = jsx:decode(FileData),
+restore_topic(Json)->
   lists:foreach(fun(I) -> 
       ID = proplists:get_value(<<"id">>,I),
       Title = proplists:get_value(<<"title">>,I),
@@ -54,3 +89,13 @@ load_topic(File)->
             ],
       ai_db:persist(topic,Item)
       end,Json).
+restore_site(Json)->
+  lists:foreach(fun(I) -> 
+    Key = proplists:get_value(<<"key">>,I),
+    Value = proplists:get_value(<<"value">>,I),
+    Item = [
+            {key,Key},
+            {value,Value}
+          ],
+      ai_db:persist(site,Item)
+    end,Json).
