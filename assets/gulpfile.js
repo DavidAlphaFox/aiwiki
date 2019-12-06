d315f081a1b1453c5f8442b3eaeafbcf38125f2c
David.Gao <david.alpha.fox@gmail.com>
2019-12-06 17:00:12 +0800

    How dropdown work

--- assets/gulpfile.js
+++ assets/gulpfile.js
@@ -7,52 +7,61 @@ var tailwindcss = require('tailwindcss');
 var postcssNested = require('postcss-nested');
 var purgecss = require('@fullhuman/postcss-purgecss');
 var cssnano = require('cssnano');
- 
+
+var isProduction = process.env.NODE_ENV === 'production';
+
 function clientCSS() {
   var plugins = [
-        postcssImport(),
-        tailwindcss(),
-        postcssNested(),
-        autoprefixer(),
-        purgecss({
+    postcssImport(),
+    tailwindcss(),
+    postcssNested(),
+    autoprefixer(),
+  ];
+  if (isProduction) {
+    plugins.push(
+      purgecss({
 
-          // Specify the paths to all of the template files in your project 
-          content: [
-            '../views/**/*.mustache'
-          ],
-        
-          // Include any special characters you're using in this regular expression
-          defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g)|| []
-        }),
-        cssnano()
-    ];
-    return gulp.src('./src/client.css')
-        .pipe(postcss(plugins))
-        .pipe(gulp.dest('../public/assets/css/'));
-}; 
+        // Specify the paths to all of the template files in your project
+        content: [
+          '../views/**/*.mustache'
+        ],
+
+        // Include any special characters you're using in this regular expression
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      }));
+  }
+
+  plugins.push(cssnano());
+  return gulp.src('./src/client.css')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('../public/assets/css/'));
+};
+
 function adminCSS() {
   var plugins = [
     postcssImport(),
     tailwindcss(),
     postcssNested(),
     autoprefixer(),
-    purgecss({
+  ];
+  if (isProduction) {
+    plugins.push(purgecss({
 
-      // Specify the paths to all of the template files in your project 
+      // Specify the paths to all of the template files in your project
       content: [
         '../aiwiki-admin/src/**/*.jsx',
         '../aiwiki-admin/public/index.html'
       ],
-    
+
       // Include any special characters you're using in this regular expression
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-      
-    }),
-  ];
-return gulp.src('./src/admin.css')
+
+    }));
+  }
+  return gulp.src('./src/admin.css')
     .pipe(postcss(plugins))
     .pipe(gulp.dest('../aiwiki-admin/src/'));
 };
 exports.clientCSS = clientCSS;
 exports.adminCSS = adminCSS;
-exports.default = gulp.parallel(clientCSS,adminCSS);
+exports.default = gulp.parallel(clientCSS, adminCSS);
