e9a1aefa8eb6f69cf487e394857cedd50c8356c8
David.Gao <david.alpha.fox@gmail.com>
2020-04-16 15:50:09 +0800

    Add links

--- src/db/db_site.erl
+++ src/db/db_site.erl
@@ -1,8 +1,21 @@
 -module(db_site).
--export([save/1]).
+-export([
+         save/1,
+         fetch/1
+        ]).
 
 -include("include/model.hrl").
 
 save(Site)->
   F = fun() -> mnesia:write(Site) end,
+  mnesia:transaction(F).
+
+fetch(links)->
+  F = fun() ->
+          MatchHead = #site{key = <<"exlinks">>, _ = '_'},
+          case mnesia:select(site,[{MatchHead,[],['$_']}]) of
+            [] -> [#site{key = <<"exlinks">>, value = #{}}];
+            Records -> Records
+          end
+      end,
   mnesia:transaction(F).
