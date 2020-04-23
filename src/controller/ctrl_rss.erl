f9df65ab2936c79b049a51d8428fff60658a53e8
David.Gao <david.alpha.fox@gmail.com>
2020-04-23 14:13:40 +0800

    upgrade site service

--- src/controller/ctrl_rss.erl
+++ src/controller/ctrl_rss.erl
@@ -3,23 +3,8 @@
 -include("include/model.hrl").
 init(Req,State)->
   {atomic,Pages} = db_page:select(0,5),
-  Site = site(Req),
+  Site = srv_site:fetch(rss),
   Body = aiwiki_rss:build(Site,Pages),
-  Req0 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/xml">>}, Body, Req),
+  Req0 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/xml">>},
+                          Body, Req),
   {ok,Req0,State}.
-site(Req)->
-  Ctx =
-    case db_site:fetch(host) of
-      {atomic,[Host]}  -> #{host => Host#site.value};
-      _ ->
-        #{host => cowboy_req:header(<<"host">>,Req,<<"/">>)}
-    end,
-  Ctx0 =
-    case db_site:fetch(brand) of
-      {atomic,[Brand]} -> Ctx#{brand => Brand#site.value};
-      _ -> Ctx
-    end,
-  case db_site:fetch(intro) of
-    {atomic,[Intro]}-> Ctx0#{intro => Intro#site.value};
-    _ -> Ctx0
-  end.
--- src/service/srv_site.erl
+++ src/service/srv_site.erl
@@ -3,19 +3,17 @@
 
 -export([fetch/1]).
 
-fetch(site)->
-  case db_site:fetch([brand,footer]) of
-    {atomic,[Site]} -> to_map(Site);
-    _ -> #{}
-  end;
-fetch(header) ->
-  case db_site:fetch([header,keywords,intro]) of
-    {atomic,[Site]} -> to_map(Site);
+fetch(site)-> to_map([brand,footer]);
+fetch(header) -> to_map([header,keywords,intro]);
+fetch(rss) -> to_map([host,brand,intro]).
+
+to_map(Keys)->
+  case db_site:fetch(Keys) of
+    {atomic,[Site]} ->
+      lists:foldl(
+        fun(I,Acc) ->
+            Key = erlang:binary_to_existing_atom(I#site.key, utf8),
+            Acc#{Key => I#site.value}
+        end, #{},Site);
     _ -> #{}
   end.
-to_map(Site)->
-  lists:foldl(
-    fun(I,Acc) ->
-        Key = erlang:binary_to_existing_atom(I#site.key, utf8),
-        Acc#{Key => I#site.value}
-    end, #{},Site).
