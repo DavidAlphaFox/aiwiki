9a94a282abdf62db2c907f5d3fefad6adbfc9a4f
David Gao <david.alpha.fox@gmail.com>
2020-07-14 23:21:53 +0800

    more page per index

--- src/utilities/aiwiki_helper.erl
+++ src/utilities/aiwiki_helper.erl
@@ -4,7 +4,7 @@
 -spec parse_pager(cowboy_req:req()) -> tuple().
 parse_pager(QS)->
   PageIndex = proplists:get_value(<<"index">>,QS,0),
-  PageCount = proplists:get_value(<<"size">>,QS,5),
+  PageCount = proplists:get_value(<<"size">>,QS,10),
   PageIndex0 = ai_string:to_integer(PageIndex),
   PageCount0 = ai_string:to_integer(PageCount),
   {PageIndex0,PageCount0}.
