50e3c89b95e19af226b2b6bca104ad801165d940
David Gao <david.alpha.fox@gmail.com>
2024-09-15 00:22:09 +0800

    to id

--- server/src/sqids.lisp
+++ server/src/sqids.lisp
@@ -6,20 +6,54 @@
 
 (in-package :aiwiki.sqids)
 
-(defparameter alphabet "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+(define-condition update-immutable-slot (error)
+  ())
 
+(defclass sqids ()
+  ((alphabet
+     :initarg :alphabet
+     :initform "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+     :accessor alphabet)))
+
+(defmethod (setf alphabet) (nval (instance sqids))
+  (error
+    (make-condition 'update-immutable-slot)))
+
+(defun string-to-chars (str)
+        (map 'list
+          #'char-code
+          (coerce str 'list)))
+(defun chars-to-string (chars)
+        (coerce
+          (map 'list
+            #'code-char
+            chars) 'string))
+;; alphabet 需要是一个list
 (defun shuffle (alphabet)
-  (let* ((l (length alphabet))
-          (o (string-to-octets alphabet))
-          (n (- l 1)))
-    (loop for i from 0
+    (loop
+      with l = (length alphabet)
+      and n = (- (length alphabet) 1)
+      and o = alphabet
+      for i from 0
       for j downfrom n to 1
       do
-      (let* ((ci (aref o i))
-             (cj (aref o j))
+      (let* ((ci (nth i o))
+             (cj (nth j o))
              (r (rem (+ ci cj (* i j)) l)))
-        (let ((cr (aref o r)))
-          (setf (aref o r ) ci)
-          (setf (aref o i) cr)
-          )))
-    (octets-to-string o)))
+        (let ((cr (nth r o)))
+          (setf (nth r o) ci)
+          (setf (nth i o) cr)))
+      finally (return o)))
+;; (chars-to-string (shuffle (string-to-chars "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")))
+;;"fwjBhEY2uczNPDiloxmvISCrytaJO4d71T0W3qnMZbXVHg6eR8sAQ5KkpLUGF9"
+
+(defun to-id (num alphabet)
+  (reverse
+    (loop with l = (length alphabet)
+      for x = num
+      then (multiple-value-bind (q _) (floor x l) q)
+      while (> x 0)
+      nconc (list (nth (rem x l) alphabet)))))
+
+;; (chars-to-string (to-id 111 (string-to-chars "fwjBhEY2uczNPDiloxmvISCrytaJO4d71T0W3qnMZbXVHg6eR8sAQ5KkpLUGF9")))
+;; w8
